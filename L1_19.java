ec2af9594d1397a6c8c73616bd7ffb964f6ae438
Malika62-sys <62739257+Malika62-sys@users.noreply.github.com>
2020-03-27 17:05:26 +0800

    Add files via upload

--- L1_19.java
+++ L1_19.java
@@ -6,7 +6,7 @@ class L1_19
 	{
 		int[] arr = {1, 5, 78, -10, -5, 0, 15, -42, -13, 14};
 		String text = "";
-		//Scanner in = new Scanner(System.in);
+		Scanner in = new Scanner(System.in);
 		int i = 0;
 		for ( i = 0; i < arr.length; i++)
 		{
